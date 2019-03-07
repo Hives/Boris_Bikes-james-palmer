1f324e99155a13e4964d04257804f567b0923a12
James <jamesdcpalmer1@gmail.com>
2019-03-07 14:33:30 +0000

    took out comment

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -14,7 +14,6 @@ class DockingStation
   end
   def dock(bike)
     fail 'Dockstation is full' if @bikes.length == 20 #capacity = 20
-     # (not future proof code as dockstation capacity can only be 1)
     @bikes.push(bike)
   end
   # def bike
